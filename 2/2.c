726f8428e24fbebc78e98a7957e8f6c894b50a19
So3c <1017354132@qq.com>
2024-03-06 22:49:46 +0100

    2

---
+++ 2/2.c
@@ -0,0 +1,28 @@
+/*The bonuses issued by the company are based on profits.
+
+When the profit (I) is less than or equal to 100,000 yuan, the bonus can be increased by 10%;
+When the profit is more than 100,000 yuan and less than 200,000 yuan, the commission is 10% for the part below 100,000 yuan, and 7.5% for the part above 100,000 yuan;
+When the amount is between 200,000 and 400,000 yuan, the portion above 200,000 yuan can be commissioned 5%;
+When the amount is between 400,000 and 600,000 yuan, the portion above 400,000 yuan can be commissioned at 3%;
+When the amount is between 600,000 and 1 million, the portion above 600,000 yuan can be commissioned 1.5%;
+When the amount exceeds 1 million yuan, the portion exceeding 1 million yuan will be charged a commission of 1%.
+Enter the current month's profit I from the keyboard. Find the total number of bonuses that should be distributed?*/
+
+
+#include <stdio.h>
+
+int main{
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
