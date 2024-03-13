dafb7f9dce3738142d7c8cc542f913378b41b05a
So3c <yuxiang.cao021@gmail.com>
2024-03-14 00:30:17 +0100

    Update 11.c

--- 11/11.c
+++ 11/11.c
@@ -1,3 +1,11 @@
+/*Classical problem (rabbits giving birth to babies):
+There is a pair of rabbits, and they give birth to a pair of
+rabbits every month from the third month after birth.
+After the rabbits grow to the third month,
+they give birth to a pair of rabbits every month.
+If the rabbits both If you don't die,
+what is the total number of rabbits every month?
+(40 months before export is sufficient)*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,11 +13,18 @@
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
-
-    return 0;
+  int x = 1, y = 0;
+  printf("month\tnumber\n");
+  for (int a = 1; a < 41; a++)
+  {
+    int sum = x + y;
+    y = x;
+    x = sum;
+    printf("%d\t%d\n", a, y);
+  }
+  return 0;
 }
