d58e5c88fc4ae4fbbc3be4f6d69d34a5db3d8939
So3c <yuxiang.cao021@gmail.com>
2024-03-14 00:30:15 +0100

    Update 10.c

--- 10/10.c
+++ 10/10.c
@@ -6,11 +6,18 @@
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
-
-    return 0;
+  printf("^^ ^^\n");
+  for (int a = 1; a < 13; a++)
+  {
+    for (int b = 1; b <= a; b++)
+    {
+      printf("%c%c", 0xa8, 0x80);
+    }
+    printf("\n");
+  }
+  return 0;
 }
