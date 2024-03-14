5f4db02ffef99030728dc38e90e7a1dd4ff3c6b4
So3c <yuxiang.cao021@gmail.com>
2024-03-14 15:19:15 +0100

    Update 12.c

--- 12/12.c
+++ 12/12.c
@@ -6,11 +6,23 @@
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
-{
+    int argc,
+    char *argv[])
 
-    return 0;
+{
+  int i, j, k;
+  for (i = 101; i < 200; i++)
+  {
+    k = sqrt(i);
+    for (j = 2; j <= k; j++)
+    {
+      if (i % j == 0)
+
+        break;
+    }
+    if (j > k)
+      printf("%d\n", i);
+  }
+  return 0;
 }
