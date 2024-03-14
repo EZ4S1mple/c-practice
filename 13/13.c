6fef5d373dfd234f90f557941d175c6c82391463
So3c <yuxiang.cao021@gmail.com>
2024-03-14 15:19:17 +0100

    Update 13.c

--- 13/13.c
+++ 13/13.c
@@ -1,3 +1,8 @@
+/*Print out all the "narcissus numbers".
+The so-called "narcissus number" refers to a three-digit number
+whose cube sum is equal to the number itself.
+For example: 153 is a "narcissus number"
+because 153=1 cubed + 5 cubed + 3 cubed.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,11 +10,18 @@
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
-
-    return 0;
+  int i, j, x, y, z;
+  for (i = 100; i < 1000; i++)
+  {
+    x = i / 100;
+    y = (i - x * 100) / 10;
+    z = i % 10;
+    if (x * x * x + y * y * y + z * z * z == i)
+      printf("%d\n", i);
+  }
+  return 0;
 }
