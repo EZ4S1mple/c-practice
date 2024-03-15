b67a2dbdaa72f0e77a84fc59ad32494cc8e89d2a
So3c <yuxiang.cao021@gmail.com>
2024-03-15 17:10:26 +0100

    Update 14.c

--- 14/14.c
+++ 14/14.c
@@ -1,4 +1,4 @@
-/*Factor a positive integer into its prime factors. 
+/*Factor a positive integer into its prime factors.
 For example: input 90, print out 90=2*3*3*5*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,11 +7,25 @@ For example: input 90, print out 90=2*3*3*5*/
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
+  int a, i;
 
-    return 0;
+  scanf("%d", &a);
+  printf("%d=", a);
+  for (i = 2; i <= a; i++)
+  {
+    while (a % i == 0)
+    {
+      a = a / i;
+      printf("%d ", i);
+
+      if (a != 1)
+        printf("* ");
+    }
+  }
+
+  return 0;
 }
