85151e1b20ed001771d87086b42af7a2ba9c00ee
So3c <yuxiang.cao021@gmail.com>
2024-03-18 21:24:35 +0100

    Update 16.c

--- 16/16.c
+++ 16/16.c
@@ -1,3 +1,5 @@
+/*Input two positive integers m and n and
+find their greatest common divisor and least common multiple.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,11 +7,38 @@
 #include <time.h>
 #include <stddef.h>
 
-
-int main(
-  int argc, 
-  char *argv[]) 
+int max(int a, int b)
 {
+  return (a > b) ? a : b;
+}
 
-    return 0;
+int min(int a, int b)
+{
+  return (a < b) ? a : b;
+}
+int main(
+    int argc,
+    char *argv[])
+{
+  int a, b;
+  scanf("%d %d", &a, &b);
+
+  for (int i = min(a, b); i > 0; i--)
+  {
+    if (a % i == 0 && b % i == 0)
+    {
+      printf("common divisor is %d\n", i);
+      break;
+    }
+  }
+
+  for (int j = max(a, b);; j++)
+  {
+    if (j % a == 0 && j % b == 0)
+    {
+      printf("least common multiple is %d\n", j);
+      break;
+    }
+  }
+  return 0;
 }
