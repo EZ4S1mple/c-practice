7594dc837b20110a66b3c55981299870a6995613
So3c <yuxiang.cao021@gmail.com>
2024-03-18 21:24:33 +0100

    Update 15.c

--- 15/15.c
+++ 15/15.c
@@ -1,3 +1,7 @@
+/*Use the nesting of conditional operators to complete this question:
+students with academic scores >= 90 points are represented by A,
+students with scores between 60-89 are represented by B,
+and students with scores below 60 are represented by C*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,11 +9,14 @@
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
-
-    return 0;
+  int s;
+  char g;
+  scanf("%d", &s);
+  g = (s >= 90) ? 'A' : ((s >= 60) ? 'B' : 'C');
+  printf("%c\n", g);
+  return 0;
 }
