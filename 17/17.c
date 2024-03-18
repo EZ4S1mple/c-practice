6279276bf47f02c5cda07b3f55939b11ff58d1d6
So3c <yuxiang.cao021@gmail.com>
2024-03-19 00:47:57 +0100

    Update 17.c

--- 17/17.c
+++ 17/17.c
@@ -1,4 +1,4 @@
-/*Enter a line of characters and count the number of 
+/*Enter a line of characters and count the number of
 English letters, spaces, numbers and other characters in it.*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,11 +7,24 @@ English letters, spaces, numbers and other characters in it.*/
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
+  char a;
+  int l = 0, s = 0, n = 0, o = 0;
+  while ((a = getchar()) != '\n')
+  {
+    if ((a <= 'z' && a >= 'a') || (a <= 'Z' && a >= 'A'))
+      l++;
+    else if (a <= '9' && a >= '0')
+      n++;
+    else if (a == ' ')
+      s++;
+    else
+      o++;
+  }
+  printf("l: %d\nn: %d\ns: %d\no: %d\n", l, n, s, o);
 
-    return 0;
+  return 0;
 }
