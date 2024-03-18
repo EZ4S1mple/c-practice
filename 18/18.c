ecf2d6a0af5a57f1f3d17c10001b460b0378e3da
So3c <yuxiang.cao021@gmail.com>
2024-03-19 00:47:58 +0100

    Update 18.c

--- 18/18.c
+++ 18/18.c
@@ -1,3 +1,7 @@
+/*Find the value of s=a+aa+aaa+aaaa+aa...a, 
+where a is a number. For example, 2+22+222+2222+22222 
+(a total of 5 numbers are added at this time), 
+and the addition of several numbers is controlled by the keyboard.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
