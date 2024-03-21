fc259ebab523a3f52d51d6017a1e3f39f072aa8c
So3c <yuxiang.cao021@gmail.com>
2024-03-21 23:02:10 +0100

    Update 18.c

--- 18/18.c
+++ 18/18.c
@@ -1,6 +1,6 @@
-/*Find the value of s=a+aa+aaa+aaaa+aa...a, 
-where a is a number. For example, 2+22+222+2222+22222 
-(a total of 5 numbers are added at this time), 
+/*Find the value of s=a+aa+aaa+aaaa+aa...a,
+where a is a number. For example, 2+22+222+2222+22222
+(a total of 5 numbers are added at this time),
 and the addition of several numbers is controlled by the keyboard.*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,11 +9,24 @@ and the addition of several numbers is controlled by the keyboard.*/
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
+  int n, d;
+  int s = 0;
+  int m = 0; // s=Σ(n*10^(d-1));
+  scanf("%d %d", &n, &d);
 
-    return 0;
+  for (int i = 1; i <= d; i++)
+  {
+    m = m * 10 + n;
+    s += m;
+    printf("%d ", m);
+    if (i < d)
+      printf("+ ");
+  }
+  printf("= %d", s);
+
+  return 0;
 }
