5f5ed595d474cc72f2116738ee08dcf781b9315a
So3c <yuxiang.cao021@gmail.com>
2024-04-23 16:40:49 +0200

    Update 24.c

--- 24/24.c
+++ 24/24.c
@@ -1,4 +1,4 @@
-/*There is a sequence of fractions: 2/1, 3/2, 5/3, 8/5, 13/8, 21/13... 
+/*There is a sequence of fractions: 2/1, 3/2, 5/3, 8/5, 13/8, 21/13...
 Find the sum of the first 20 terms of this sequence.*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,11 +7,20 @@ Find the sum of the first 20 terms of this sequence.*/
 #include <time.h>
 #include <stddef.h>
 
-
 int main(
-  int argc, 
-  char *argv[]) 
+    int argc,
+    char *argv[])
 {
+  float a = 2, b = 1;
+  double sum = 0, c;
+  for (int i = 0; i < 20; i++)
+  {
 
-    return 0;
+    sum = sum + a / b;
+    c = a + b;
+    b = a;
+    a = c;
+  }
+  printf("%f", sum);
+  return 0;
 }
