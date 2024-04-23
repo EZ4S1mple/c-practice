5bb9bf2d23d2d1e26d7a6fc5d19a2745e80b334e
So3c <yuxiang.cao021@gmail.com>
2024-04-23 16:40:50 +0200

    Update 25.c

--- 25/25.c
+++ 25/25.c
@@ -1,3 +1,4 @@
+//Find the sum of 1+2!+3!+...+20!
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,6 +11,11 @@ int main(
   int argc, 
   char *argv[]) 
 {
-
+double sum=0,i,j=1;
+for (i=1;i<=20;i++){
+  j=j*i;
+  sum=j+sum;
+}
+printf("%lf",sum);
     return 0;
 }
